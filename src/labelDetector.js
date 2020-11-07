075ae97766950847f0b607fccc8da633881e098b
apangasa <60531810+apangasa@users.noreply.github.com>
2020-11-07 17:30:59 -0500

    Trying to use base64

--- src/labelDetector.js
+++ src/labelDetector.js
@@ -1,9 +1,30 @@
 
 async function detectLabels(path) {
     const vision = require('@google-cloud/vision');
+    var fs = require('fs');
+    var imageFile = fs.readFileSync(path);
+
+    var encoded = Buffer.from(imageFile).toString('base64');
+    console.log(typeof encoded)
+
+    let request_body_json = {
+        "requests":[
+            {
+              "image":{
+                "content":encoded
+              },
+              "features":[
+                {
+                  "type":"LABEL_DETECTION",
+                  "maxResults":1
+                }
+              ]
+            }
+          ]
+    }
 
     const client = new vision.ImageAnnotatorClient();
-    const [result] = await client.labelDetection(path);
+    const [result] = await client.labelDetection(request_body_json);
     const labels = result.labelAnnotations;
 
     let boiled = false;
@@ -17,4 +38,4 @@ async function detectLabels(path) {
     return boiled;
 }
 
-detectLabels('C:\\Users\\arnav\\OneDrive - University of Florida\\whack-ding\\resources\\water9.jpg');
+detectLabels('C:\\Users\\arnav\\OneDrive - University of Florida\\whack-ding\\resources\\water9.png');
